10cf6fe42508aae57680a1836c10f4799185c66b
EDockhorn <johnnyerick111@hotmail.com>
2021-05-25 23:26:21 -0300

    Initial Commit

---
+++ POC.FileSizeCount/Program.cs
@@ -0,0 +1,23 @@
+﻿using Humanizer.Bytes;
+using System;
+using System.IO;
+
+namespace POC.FileSizeCount
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            long size = 0;
+            DirectoryInfo dir = new DirectoryInfo(@"\.folder");
+            foreach (FileInfo fi in dir.GetFiles("*.*", SearchOption.AllDirectories)) size += fi.Length;
+            
+
+            var maxFileSize = ByteSize.FromBytes(size);            
+            Console.WriteLine($"maxFileSize.Bytes: {maxFileSize.Bytes:#.##}");
+            Console.WriteLine($"maxFileSize.Kilobytes: {maxFileSize.Kilobytes:#.##}");
+            Console.WriteLine($"maxFileSize.Megabytes: {maxFileSize.Megabytes:#.##}");
+            Console.WriteLine($"maxFileSize.Gigabytes: {maxFileSize.Gigabytes:#.##}");
+        }
+    }
+}
